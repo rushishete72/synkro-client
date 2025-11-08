4b530cdd3dcb8e30e4577b197ac37b8128e3f757
rushi <rushishete72@gmail.com>
2025-11-08 11:16:45 +0530

    Initial React setup with about and zooh.htm

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+// src/About.jsx
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>About Synkro</h1>
+      <p>This is the about page for Synkro.in</p>
+    </div>
+  );
+}
